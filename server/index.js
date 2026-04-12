20d1c182d3d5d76fcea07492dae0c210dfe4e4bf
Ashish Kumar <ashishkumarr0856@gmail.com>
2026-04-12 16:24:13 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from "dotenv";
 import connectDB from './database/db.js';
+import userRoute from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({});
 
@@ -9,6 +12,23 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//default middleware
+app.use(express.json());
+app.use(cookieParser({
+    origin: "http://localhost:8080",
+    Credential:true
+}));
+app.use(cors())
+// apis
+app.use("/api/v1/user", userRoute);
+
+// app.get("/home",(_,res) => {
+//   return  res.status(200).json({
+//         success:true,
+//         message: "hello I am comming from backend"
+//     })
+// })
 
 app.listen(PORT, () => {
     console.log(`server listen at port ${PORT}`);
