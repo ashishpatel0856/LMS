c8b9a1587ca3f07b3b5d8acf246c28299d33e03e
Ashish Patel <ashishkumarr0856@gmail.com>
2026-04-18 21:40:37 +0530

    dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,8 +1,18 @@
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className=' grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+      <Card>
+        <CardHeader className='items-center'>
+          <CardTitle>Total sales</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-2xl font-bold">₹0</p>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
